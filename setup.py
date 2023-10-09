6111d8b8e4705f09969a2bb8989996bc1f29f0ee
Fabien Moreau <fabien@lighton.ai>
2023-10-09 16:51:23 +0200

    Loosen requirements versions.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     url="https://github.com/lightonai/paradigm-client",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
-    install_requires=["aiohttp>=3.8.4", "pydantic>=1.10.2", "requests>=2.28.2", "tqdm>=4.66.1"],
+    install_requires=["aiohttp>=3.8.4", "pydantic>=1.10.2", "requests>=2.31.0", "tqdm>=4.64.1"],
     extras_require={"langchain": ["langchain>=0.0.306"]},
     packages=find_packages(exclude=["examples", "tests"]),
     keywords=["NLP", "API", "AI", "LLM"],
